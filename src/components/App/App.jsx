99cc24b5c52495c7512d4428c0cb67e221890c94
kevinwitherspoon <kevofbe6@icloud.com>
2025-01-10 08:53:47 -0700

    last correction

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -99,7 +99,7 @@ function App() {
         </fieldset>
       </ModalWithForm>
       <ItemModal
-        activeModal={activeModal}
+        isOpen={activeModal === "preview"}
         card={selectedCard}
         onClose={closeActiveModal}
       />
