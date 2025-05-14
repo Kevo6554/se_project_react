18c8ee36c5bd2c0e600047c9fa6511fb775808ae
kevinwitherspoon <kevofbe6@icloud.com>
2025-05-14 11:22:46 -0700

    project submit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,26 @@ import logo from "../../images/logo.svg";
 import avatar from "../../images/avatar.png";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 import { Link } from "react-router-dom";
+import { useState, useContext } from "react";
+import { CurrentUserContext } from "../../context/CreateUserContext";
 
-function Header({ handleAddClick, weatherData }) {
+function Header({
+  handleAddClick,
+  weatherData,
+  handleRegistration,
+  handleLogin,
+  handleSignOut,
+}) {
+  const currentUser = useContext(CurrentUserContext);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const currentDate = new Date().toLocaleString("default", {
     month: "long",
     day: "numeric",
   });
+
+  const isLoggedIn = !!currentUser?._id;
+
+  const toggleMenu = () => setIsMenuOpen((prev) => !prev);
   return (
     <header className="header">
       <Link to="/">
@@ -17,20 +31,68 @@ function Header({ handleAddClick, weatherData }) {
       <p className="header__date-and-loaction">
         {currentDate}, {weatherData.city}
       </p>
-      <ToggleSwitch />
       <button
-        onClick={handleAddClick}
+        className="header__menu-button"
         type="button"
-        className="header__add-clothes-btn"
+        onClick={toggleMenu}
+      ></button>
+      <div
+        className={`header__mobile-menu ${
+          isMenuOpen ? "header__mobile-menu_opened" : ""
+        }`}
       >
-        + Add clothes
-      </button>
-      <Link to="/profile" className="header__link">
-        <div className="header__user-container">
-          <p className="header__username">Terrence Tegegne</p>
-          <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
-        </div>
-      </Link>
+        <button
+          className="header__mobile-menu_close"
+          type="button"
+          onClick={toggleMenu}
+        ></button>
+        <ToggleSwitch />
+        {isLoggedIn ? (
+          <>
+            <button
+              onClick={handleAddClick}
+              type="button"
+              className="header__add-clothes-btn"
+            >
+              + Add clothes
+            </button>
+            <button
+              onClick={handleSignOut}
+              type="button"
+              className="header__button-logout"
+            >
+              Log Out
+            </button>
+            <Link to="/profile" className="header__link">
+              <div className="header__user-container">
+                <p className="header__username">Terrence Tegegne</p>
+                <img
+                  src={avatar}
+                  alt="Terrence Tegegne"
+                  className="header__avatar"
+                />
+              </div>
+            </Link>
+          </>
+        ) : (
+          <>
+            <button
+              onClick={handleLogin}
+              className="header__button-login"
+              type="button"
+            >
+              Log In
+            </button>
+            <button
+              onClick={handleRegistration}
+              className="header__button-register"
+              type="button"
+            >
+              Sign Up
+            </button>
+          </>
+        )}
+      </div>
     </header>
   );
 }
